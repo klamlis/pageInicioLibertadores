d01c044acdb69a4106c88a38072b4ec062b3a74c
klamlis <klamlis@gmail.com>
2014-09-24 13:45:31 -0500

    SpotLight
    Version SpotLight

---
+++ spotLight/footer_edge.js
@@ -0,0 +1,77 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+var opts = {
+    'gAudioPreloadPreference': 'auto',
+
+    'gVideoPreloadPreference': 'auto'
+};
+var resources = [
+];
+var symbols = {
+"stage": {
+    version: "4.0.1",
+    minimumCompatibleVersion: "4.0.1",
+    build: "4.0.1.365",
+    baseState: "Base State",
+    scaleToFit: "none",
+    centerStage: "none",
+    initialState: "Base State",
+    gpuAccelerate: false,
+    resizeInstances: false,
+    content: {
+            dom: [
+            {
+                id: 'Text',
+                type: 'text',
+                rect: ['10px', '14px','526px','76px','auto', 'auto'],
+                text: "texto",
+                font: ['Arial, Helvetica, sans-serif', 12, "rgba(0,0,0,1)", "normal", "none", ""]
+            }],
+            symbolInstances: [
+
+            ]
+        },
+    states: {
+        "Base State": {
+            "${_Stage}": [
+                ["color", "background-color", 'rgba(255,255,255,1)'],
+                ["style", "width", '547px'],
+                ["style", "height", '100px'],
+                ["style", "overflow", 'hidden']
+            ],
+            "${_Text}": [
+                ["style", "width", '526px'],
+                ["style", "left", '10px'],
+                ["style", "font-size", '12px']
+            ]
+        }
+    },
+    timelines: {
+        "Default Timeline": {
+            fromState: "Base State",
+            toState: "",
+            duration: 0,
+            autoPlay: true,
+            timeline: [
+            ]
+        }
+    }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources, opts);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-18247303");
